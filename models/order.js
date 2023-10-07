74055d76c3be561052d804531eada2b8fb3ff9b9
shubhamadav <shubhamyad100@gmail.com>
2023-10-08 03:16:22 +0530

    Order Show

--- models/order.js
+++ models/order.js
@@ -1,14 +1,28 @@
-const Sequelize = require('sequelize');
+const mongoose = require('mongoose')
 
-const sequelize = require('../util/database');
+const Schema = mongoose.Schema;
 
-const Order = sequelize.define('order', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  }
+const orderSchema = new Schema ({
+    products : [{product:{
+      type:String,
+      required: true
+ },
+ quantity:{
+      type:String,
+      required: true
+ }
+}],
+     user :{
+        name:{
+           type :String,
+           required: true
+        },
+        userId :{
+            type : Schema.Types.ObjectId,
+            required: true,
+            ref:'User'
+        }
+     }
 });
 
-module.exports = Order;
+module.exports = mongoose.model('Order' , orderSchema)
